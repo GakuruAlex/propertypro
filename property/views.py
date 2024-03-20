43027533b43087f614746d58288c7f80fa76a7ca
alexgicimo <gakurualexgicimo@gmail.com>
2024-03-20 18:58:19 +0000

    Home view

--- property/views.py
+++ property/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from .models import Property,House,WaterBill,ElectricBill
+from django.contrib import messages
 # Create your views here.
+def home(request):
+    try:
+        properties = Property.objects.all()
+    except Exception as e:
+        messages.error(request,f"Error: {e}")
+        return render (request,'property/home.html')
+    
+    return render(request,'property/home.html',{'properties':properties})
