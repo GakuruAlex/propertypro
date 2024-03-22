ace3101fde46b2321448096e329de387c358d12f
alexgicimo <gakurualexgicimo@gmail.com>
2024-03-22 23:19:29 +0000

    Tailored forms to modelchoicefield and modelmultiplechoicefield

--- property/forms.py
+++ property/forms.py
@@ -1,9 +1,9 @@
-from django.forms import forms
+from django import forms
 from .models import House
 from accounts.models import Owner
-class PropertyForm(forms.Forms):
-    name = forms.CharField( max_length=25, required=True)
+class PropertyForm(forms.Form):
+    name = forms.CharField(max_length=25, required=False)
     location = forms.CharField( max_length=25, required=True)
     no_houses = forms.IntegerField( required=True)
-    houses = forms.Foreignkey(House.house_number)
-    owner =forms.ManyToManyField(Owner.name)
+    houses = forms.ModelChoiceField(queryset=House.objects.all())
+    owner =forms.ModelMultipleChoiceField(queryset=Owner.objects.all())
