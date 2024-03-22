0daf711b814be5c46920e53caab8bc97b04d456b
alexgicimo <gakurualexgicimo@gmail.com>
2024-03-22 22:33:26 +0000

    Register Bills in admin dashboard for mockup

--- property/admin.py
+++ property/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import House
+from .models import House,Bill,WaterBill,ElectricBill
 # Register your models here.
 admin.site.register(House)
+admin.site.register(Bill)
+admin.site.register(WaterBill)
+admin.site.register(ElectricBill)
