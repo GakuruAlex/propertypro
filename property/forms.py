28911e2bcf4c75e0b4e6ecf4502fc7d287149367
alexgicimo <gakurualexgicimo@gmail.com>
2024-03-22 22:36:24 +0000

    PropertyForm1

--- property/forms.py
+++ property/forms.py
@@ -1,7 +1,7 @@
 from django.forms import forms
 from .models import House
 from accounts.models import Owner
-class ProductForm(forms.Forms):
+class PropertyForm(forms.Forms):
     name = forms.CharField( max_length=25, required=True)
     location = forms.CharField( max_length=25, required=True)
     no_houses = forms.IntegerField( required=True)
