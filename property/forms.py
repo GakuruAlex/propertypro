93e3dc514f9438f830dde4c78470ec5095de073d
alexgicimo <gakurualexgicimo@gmail.com>
2024-03-22 22:36:07 +0000

    PropertyForm

---
+++ property/forms.py
@@ -0,0 +1,9 @@
+from django.forms import forms
+from .models import House
+from accounts.models import Owner
+class ProductForm(forms.Forms):
+    name = forms.CharField( max_length=25, required=True)
+    location = forms.CharField( max_length=25, required=True)
+    no_houses = forms.IntegerField( required=True)
+    houses = forms.Foreignkey(House.house_number)
+    owner =forms.ManyToManyField(Owner.name)
